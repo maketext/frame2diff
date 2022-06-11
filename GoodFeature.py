a535780ebf50576efa8134e89f2d15e210700b87
L. Chang <32004044+maketext@users.noreply.github.com>
2022-06-11 14:37:24 +0900

    GoodFeature 테스트용
    GoodFeature 테스트용

--- GoodFeature.py
+++ GoodFeature.py
@@ -1,33 +1,9 @@
 import cv2 #open-cv
-# import paho.mqtt.client as mClient
-# import time
-# import threading
-# import random
-import os # 현재사용os에서 가능 파일위치등
+import os # 현재사용 os 에서 가능 파일위치 등
 from enum import Enum
-
-#from cv2 import imwrite
-# from sys import exit
   
 class KeyCode(Enum):
     Enter = 13
-
-# def imwrite(filename, img, params=None):
-   # try: 
-     #   ext = os.path.splitext(filename)[1]
-      #  result, n = cv2.imencode(ext, img, params)
-       # if result:  
-         #   with open(filename, mode='w+b') as f:
-          #       n.tofile(f)
-           #      return True
-         #else:
-          #   return False
-     #except Exception as e:
-      #   print(e)
-       #  return False
-
-
-
 
 cap = None
 count = 0
