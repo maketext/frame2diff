158ea113d0685d247e84a77976980e64f44d10f3
L. Chang <32004044+maketext@users.noreply.github.com>
2023-02-02 08:07:41 +0900

    Update matching.py

--- matching.py
+++ matching.py
@@ -1,19 +1,34 @@
 import cv2
 
-im = cv2.imread('img/bolt1.jpg', cv2.IMREAD_GRAYSCALE)
-bg = cv2.imread('img/bolts.jpg', cv2.IMREAD_GRAYSCALE)
+bg = cv2.imread('img/mold.png', cv2.IMREAD_GRAYSCALE) # 원본 이미지
+bg = cv2.resize(bg, (100, 100))
+im = bg[0:33, 33:66] # 탬플릿 이미지
 w, h = im.shape[:2]
 
-res = cv2.matchTemplate(bg, im, cv2.TM_CCOEFF_NORMED)
+print(w, h)
+parts = [bg[0:33, 0:33], bg[0:33, 33:66], bg[0:33, 66:99],
 
-for y in range(0, res.shape[0]):
-    for x in range(0, res.shape[1]):
-        if res[y,x] > 0.7:
-            cv2.rectangle(bg, (x, y), (x + w, y + h), (0, 0, 255), 1)
+        bg[33:66, 0:33], bg[33:66, 66:99],
+
+        bg[66:99, 0:33], bg[66:99, 33:66], bg[66:99, 66:99]
+]
+crop = [[0,33,0,33], [0,33,33,66], [0,33,66,99],
+        [33,66,0,33], [33,66,33,66],
+        [66,99,0,33], [66,99,33,66],[66,99,66,99]
+        ]
+for i, part in enumerate(parts):
+    print(part.shape)
+    res = cv2.matchTemplate(part, im, cv2.TM_CCOEFF_NORMED) # 원본, 탬플릿 이미지, 옵션 순
+
+    for y in range(0, res.shape[0]):
+        for x in range(0, res.shape[1]):
+            if res[y,x] > 0.5:
+                y1, y2, x1, x2 = crop[i]
+                checked = cv2.cvtColor(bg, cv2.COLOR_GRAY2RGB)
+                checked = cv2.rectangle(checked, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.imshow("ddd", checked)
+                cv2.imwrite("ddd.jpg", checked)
 
 #res = res * 255
 
-cv2.imshow('bolt1', im)
-cv2.imshow('match', res)
-cv2.imshow('result', bg)
 cv2.waitKey(0)
